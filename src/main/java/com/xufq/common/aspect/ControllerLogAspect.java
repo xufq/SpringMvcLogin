808a250f0e548a0f51c9db77c340fcea5dd687f3
xufq <xufq0817>
2018-03-14 23:08:27 +0800

    引入logback 完成前后台联调

---
+++ src/main/java/com/xufq/common/aspect/ControllerLogAspect.java
@@ -0,0 +1,51 @@
+package com.xufq.common.aspect;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.xufq.common.annotation.ControllerLog;
+
+@Aspect
+public class ControllerLogAspect {
+
+	// 切入点
+	@Pointcut(value = "@annotation(com.xufq.common.annotation.ControllerLog)")
+	private void pointcut() {
+
+	}
+
+	/**
+	 * 在方法执行前后
+	 *
+	 * @param point
+	 * @param myLog
+	 * @return
+	 */
+	@Around(value = "pointcut() && @annotation(log)")
+	public Object printLog(ProceedingJoinPoint point, ControllerLog log) {
+		String url = log.requestUrl();
+		Class clazz = point.getTarget().getClass();
+		Logger logger = getLogger(clazz);
+		try {
+			logger.debug("请求URL:" + url);
+			Object[] args = point.getArgs();
+			if (args != null && args.length > 0) {
+				for (int i = 0; i < args.length; i++) {
+					logger.debug("请求参数：" + args[i].toString());
+				}
+			}
+			return point.proceed(); // 执行程序
+		} catch (Throwable throwable) {
+			throwable.printStackTrace();
+			return throwable.getMessage();
+		}
+	}
+
+	private Logger getLogger(Class clazz) {
+		return LoggerFactory.getLogger(clazz);
+	}
+}
--- src/main/java/com/xufq/sys/login/controller/LoginController.java
+++ src/main/java/com/xufq/sys/login/controller/LoginController.java
@@ -4,10 +4,13 @@
 package com.xufq.sys.login.controller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.xufq.common.annotation.ControllerLog;
 import com.xufq.sys.login.vo.UserInfoVO;
+import com.xufq.sys.result.Result;
 
 /**
  * @author Administrator
@@ -16,13 +19,16 @@ import com.xufq.sys.login.vo.UserInfoVO;
 @RestController
 @RequestMapping("/login")
 public class LoginController {
-
 	@PostMapping("/login")
-	public String login(UserInfoVO userInfoVO) {
-		// 用户名密码验证
-		if(!"admin".equals(userInfoVO.getUserId()) || !"admin".equals(userInfoVO.getPassword())) {
-			return "redirect:/error.html";
+	@ControllerLog(requestUrl = "/login")
+	public Result login(@RequestBody UserInfoVO userInfoVO) {
+		Result result = new Result();
+		// 鐧诲綍楠岃瘉
+		if ("admin".equals(userInfoVO.getUserId()) && "admin".equals(userInfoVO.getPassword())) {
+			result.setSuccess(true);
+		} else {
+			result.setSuccess(false);
 		}
-		return "redirect:/index.html";
+		return result;
 	}
 }
