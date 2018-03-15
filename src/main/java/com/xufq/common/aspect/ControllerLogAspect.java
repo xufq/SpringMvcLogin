30c45447a73a0639f0ebb926eaf6f9c17a79355b
xufq <xufq0817>
2018-03-15 23:59:58 +0800

    logback配置修改和Controller层log注解追加

--- src/main/java/com/xufq/common/aspect/ControllerLogAspect.java
+++ src/main/java/com/xufq/common/aspect/ControllerLogAspect.java
@@ -6,16 +6,17 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 
 import com.xufq.common.annotation.ControllerLog;
 
 @Aspect
+@Component
 public class ControllerLogAspect {
 
 	// 切入点
 	@Pointcut(value = "@annotation(com.xufq.common.annotation.ControllerLog)")
 	private void pointcut() {
-
 	}
 
 	/**
